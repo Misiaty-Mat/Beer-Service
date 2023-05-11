aba2fa3d2de819e9e65c579aaf5681d0b3e4dd20
Misiaty-Mat <m.a.s.m@interia.pl>
2023-05-11 20:08:40 +0200

    Seed database with CSV data

---
+++ src/main/java/com/spring6framework/spring6restmvc/services/BeerCsvService.java
@@ -0,0 +1,10 @@
+package com.spring6framework.spring6restmvc.services;
+
+import com.spring6framework.spring6restmvc.model.BeerCSVRecord;
+
+import java.io.File;
+import java.util.List;
+
+public interface BeerCsvService {
+    List<BeerCSVRecord> convertCSV(File csvFile);
+}
