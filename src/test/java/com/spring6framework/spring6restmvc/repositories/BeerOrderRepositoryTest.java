b24539badac0c14e0cba64edd107be341be9d8f6
Misiaty-Mat <m.a.s.m@interia.pl>
2023-05-15 10:07:55 +0200

    Beer order data model

---
+++ src/test/java/com/spring6framework/spring6restmvc/repositories/BeerOrderRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.spring6framework.spring6restmvc.repositories;
+
+import com.spring6framework.spring6restmvc.entities.Beer;
+import com.spring6framework.spring6restmvc.entities.BeerOrder;
+import com.spring6framework.spring6restmvc.entities.BeerOrderShipment;
+import com.spring6framework.spring6restmvc.entities.Customer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+class BeerOrderRepositoryTest {
+
+    @Autowired
+    BeerOrderRepository beerOrderRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    Customer testCustomer;
+    Beer testBeer;
+
+    @BeforeEach
+    void setUp() {
+        testCustomer = customerRepository.findAll().get(0);
+        testBeer = beerRepository.findAll().get(0);
+    }
+
+    @Transactional
+    @Test
+    void testBeerOrders() {
+        BeerOrder beerOrder = BeerOrder.builder()
+                .beerOrderShipment(BeerOrderShipment.builder()
+                        .trackingNumber("1235r")
+                        .build())
+                .customerRef("Test order")
+                .customer(testCustomer)
+                .build();
+
+        BeerOrder savedBeerOrder = beerOrderRepository.save(beerOrder);
+
+        System.out.println(savedBeerOrder.getCustomerRef());
+    }
+}
---
+++ src/test/java/com/spring6framework/spring6restmvc/repositories/CategoryRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.spring6framework.spring6restmvc.repositories;
+
+import com.spring6framework.spring6restmvc.entities.Beer;
+import com.spring6framework.spring6restmvc.entities.Category;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CategoryRepositoryTest {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    BeerRepository beerRepository;
+    Beer testBeer;
+
+    @BeforeEach
+    void setUp() {
+        testBeer = beerRepository.findAll().get(0);
+    }
+
+    @Transactional
+    @Test
+    void testAddCategory() {
+        Category savedCategory = categoryRepository.save(Category.builder()
+                        .description("Ales")
+                .build());
+
+        testBeer.addCategory(savedCategory);
+        Beer savedBeer = beerRepository.save(testBeer);
+
+        System.out.println(savedBeer);
+    }
+}
