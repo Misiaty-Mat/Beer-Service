f30b9fefce91554fe2fb12c8e26d25d5b15718e2
Misiaty-Mat <m.a.s.m@interia.pl>
2023-09-18 11:31:32 +0200

    Add OpenApi3 support

--- src/main/java/com/spring6framework/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/com/spring6framework/spring6restmvc/config/SpringSecConfig.java
@@ -2,15 +2,19 @@ package com.spring6framework.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Profile("!test")
 @Configuration
 public class SpringSecConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
+                .requestMatchers("/v3/api-docs**", "/swagger-ui/**", "/swagger-ui.html")
+                .permitAll()
                 .anyRequest().authenticated()
                 .and().oauth2ResourceServer().jwt();
 
